9ad059b9bb9641941444e860699bbcd839c747d7
gusmfs <oigostavo@gmail.com>
2023-10-17 17:03:33 -0300

    das

--- src/routes/course.routes.ts
+++ src/routes/course.routes.ts
@@ -2,8 +2,8 @@ import { Router } from "express";
 import { validBody } from "../middlewares/validBody";
 import { validToken } from "../middlewares/validToken.middleware";
 import { courseCreateSchema } from "../schemas/courses.schema";
-import { createCourseController } from "../controllers/course.controller";
+import { createCourseController, getAllCourseController } from "../controllers/course.controller";
 import { verifyOwnerAndAdminPermission } from "../middlewares/verifyOwnerAndAdminPermission.middleware";
 export const courseRoutes : Router = Router()
 courseRoutes.post('/', validToken, verifyOwnerAndAdminPermission ,validBody(courseCreateSchema), createCourseController)
-courseRoutes.get('/')
+courseRoutes.get('/', getAllCourseController)
