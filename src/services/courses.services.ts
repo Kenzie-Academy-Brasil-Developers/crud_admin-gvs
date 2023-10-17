a9ab69a6179c15dda99f5405218d93d721d1f841
gusmfs <oigostavo@gmail.com>
2023-10-16 23:40:50 -0300

    fea=a

--- src/services/courses.services.ts
+++ src/services/courses.services.ts
@@ -1,7 +1,6 @@
 import format from "pg-format";
 import { client } from "../database";
 import { TCourse, TCourseCreate, TCourseResult } from "../interfaces/course.interface";
-import { courseCreateSchema } from "../schemas/courses.schema";
 
 export const courseCreateService = async (data:TCourseCreate): Promise<TCourse> => {
     const queryFormat : string = format(`INSERT INTO courses (%I) VALUES (%L) RETURNING *;`,
