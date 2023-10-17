d4d2f6c7f4fe2889b96a325f1b74a8adf14ec6c9
gusmfs <oigostavo@gmail.com>
2023-10-17 17:08:33 -0300

    kk

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,10 +2,10 @@ import { Router } from "express";
 import { createUserController, getAllUsersController } from "../controllers/users.controller";
 import { validBody } from "../middlewares/validBody";
 import { validToken } from "../middlewares/validToken.middleware";
-import { verifyPermission } from "../middlewares/verifyOwnerAndAdminPermission.middleware";
+import { verifyOwnerAndAdminPermission} from "../middlewares/verifyOwnerAndAdminPermission.middleware";
 import { userCreateSchema } from "../schemas/users.schema";
 
 export const userRoutes : Router = Router()
 userRoutes.post('/',validBody(userCreateSchema), createUserController)
-userRoutes.get('/', validToken, verifyPermission ,getAllUsersController)
+userRoutes.get('/', validToken, verifyOwnerAndAdminPermission ,getAllUsersController)
 userRoutes.get('/:id/courses',)
