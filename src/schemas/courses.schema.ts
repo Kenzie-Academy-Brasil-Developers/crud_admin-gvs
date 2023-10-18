afd78b16261173bbbcf3299a43e850f5fb678c4b
gusmfs <oigostavo@gmail.com>
2023-10-18 16:50:04 -0300

    adassa

--- src/schemas/courses.schema.ts
+++ src/schemas/courses.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 export const courseSchema = z.object({
     id : z.number().positive(),
     name : z.string().max(15),
-    description :z.string().max(15)
+    description :z.string()
 })
 export const courseCreateSchema = courseSchema.omit({id:true})
 export const courseReadSchema = courseSchema.array()
