a59b2120bb5a9680fd88ce71c8fe7b7c3788903c
gusmfs <oigostavo@gmail.com>
2023-10-17 22:44:56 -0300

    asda

--- src/routes/session.routes.ts
+++ src/routes/session.routes.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { loginController } from "../controllers/session.controller";
 import { validBody } from "../middlewares/validBody";
 import { sessionSchema } from "../schemas/session.schema";
-import { userRoutes } from "./user.routes";
 
 export const sessionRoutes : Router = Router()
 sessionRoutes.post('/', validBody(sessionSchema),loginController )
